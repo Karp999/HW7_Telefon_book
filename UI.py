cde3d67ec026e01d638660b1c45749b90150531f
Olga <osa_007@mail.ru>
2022-10-19 19:05:41 +0300

    added UI

---
+++ UI.py
@@ -0,0 +1,25 @@
+import search
+import write_read_delete_change
+
+
+def menu():
+    m = input('''Поиск контакта: введите 1.
+             \nРедактировать контакт: введите 2.
+             \nУдалить контакт: введите 3. 
+             \nСоздать новую запись, сохранить в строку: введите 4. 
+             \nСоздать новую запись, сохранить в столбец: введите 5. 
+             \nВыход: введите 0.\n''')
+
+    match m:
+        case '1':
+            return search.search()
+        case '2':
+            return
+        case '3':
+            return
+        case '4':
+            return write_read_delete_change.write_file_string()
+        case '5':
+            return write_read_delete_change.write_file_column()
+
+menu()
