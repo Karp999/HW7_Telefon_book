36403c8bf7d3dc89129a0b01fe76dea66b3b4131
Olga <osa_007@mail.ru>
2022-10-21 00:18:36 +0300

    added main

--- delete_change.py
+++ delete_change.py
@@ -1,45 +1,17 @@
-import write_read
+import re
+import logger
 
-def search():
-    surname = input('Введите фамилию: ')
-    file = write_read.read_file_string()
-    new_file = file.split()
-    #print(new_file)
-    if surname in new_file:
-        for i in range(0, len(new_file)-1):
-            if surname == new_file[i]:
-                print("yes")
-                del new_file[i]
-                # del new_file[i+1]
-                # del new_file[i+2]
-                # del new_file[i+3]
-    else:
-       print('Контакт не найден') 
-
-#search()
+def delete():
+    with open ('Phonebook_string.csv', 'r', encoding='utf-8', newline='') as file, open  ('Phonebook_column.csv', 'r', encoding='utf-8', newline='') as file2:
+        lst = file.readlines()
+        lst2 = file2.readlines()
+        surname = input('Введите фамилию: ')
+        pattern = re.compile(re.escape(surname))
+        with open('Phonebook_string.csv', 'w', encoding='utf-8', newline='') as f:
+            for i in lst:
+                result = pattern.search(i)
+                if result is None:
+                    f.write(i)
+    logger.info_logger(f'Удаление записи: {pattern}')
 
 
-
-
-# import search
-# import write_read_delete_change
-
-# def delete():
-#     del_record = search.search()
-#     new_del_record = list.append(del_record)
-#     print(type(new_del_record))
-#     file = write_read_delete_change.read_file_string()
-#     new_file = file.split()
-#     for i in range(len(new_file)):
-#         if new_del_record[0] == new_file[i]:
-#                 #print(new_file [i], new_file[i+1], new_file[i+2], new_file[i+3])
-#             print("hd")
-#         else:
-#             print("f")   
-        
-#         # for i in range(len(new_file)):
-#         #     if surname == new_file[i]:
-#         #         print(new_file [i], new_file[i+1], new_file[i+2], new_file[i+3])
-# #delete()    
-    
-    
--- write_read.py
+++ write_read.py
@@ -1,4 +1,5 @@
 from get_info import get_info
+import logger
 
 # записали введенные данные в файл в строку
 
@@ -6,6 +7,7 @@ def write_file_string():
     info = get_info()
     with open('Phonebook_string.csv', 'a', encoding='utf-8', newline='') as file:
         file.write(f'{info[0]},{info[1]},{info[2]},{info[3]}\n')
+    logger.info_logger(f'Новая запись в тел.книгу 1: {info}')
 
 # записали введенные данные в файл в столбец
 
@@ -13,7 +15,7 @@ def write_file_column():
     info = get_info()
     with open('Phonebook_column.csv', 'a', encoding='utf-8', newline='') as file:
         file.write(f'{info[0]}\n{info[1]}\n{info[2]}\n{info[3]}\n\n')
-#write_file_column()
+    logger.info_logger(f'Новая запись в тел.книгу 2: {info}')
 
 # Считываем из файла построчно
 
@@ -23,15 +25,9 @@ def read_file_string():
         some_list = list.replace(','," ")
         return(some_list)
         
-
-
-# Считываем из файла столбцами  Зачем???
+# Считываем из файла столбцами
 
 def read_file_column():
     with open ('Phonebook_column.csv', 'r', encoding='utf-8', newline='') as file:
         list = file.read()
         return(list)
-
-# def print_file():
-#     print ()
-
