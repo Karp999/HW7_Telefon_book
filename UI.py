c3977c397cfe9a4fbd82b3cf22d270828f38a0f6
Anna.K <kkk.aanya1996@mail.ru>
2022-10-21 22:39:19 +0300

    добавила change  в меню (мы удаляли его)

--- UI.py
+++ UI.py
@@ -1,13 +1,15 @@
 import search
 import write_read
 import delete
+import change
 
 def menu():
     m = input('''Поиск контакта в телефонной книге 1 (строка): введите 1.
-             \nПоиск контакта в телефонной книге 2 (столбец): введите 2
-             \nУдалить контакт: введите 3. 
-             \nСоздать новую запись, сохранить в телефонной книге 1 (строка): введите 4. 
-             \nСоздать новую запись, сохранить в в телефонной книге 2 (столбец): введите 5. 
+             \nПоиск контакта в телефонной книге 2 (столбец): введите 2.
+             \nРедактировать контакт: введите 3.
+             \nУдалить контакт: введите 4. 
+             \nСоздать новую запись, сохранить в телефонной книге 1 (строка): введите 5. 
+             \nСоздать новую запись, сохранить в в телефонной книге 2 (столбец): введите 6. 
              \nВыход: введите 0.\n''')
 
     match m:
@@ -16,10 +18,13 @@ def menu():
         case '2':
             return search.search_column()
         case '3':
+            return change.askAboutChanging()
             return
         case '4':
-            return write_read.write_file_string()
+            return delete.delete()    
         case '5':
+            return write_read.write_file_string()
+        case '6':
             return write_read.write_file_column()
         case '0':
             return
