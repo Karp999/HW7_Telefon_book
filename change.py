8266ded32acf375e60199c9022ac806f1bcfacbe
Anna.K <kkk.aanya1996@mail.ru>
2022-10-21 19:31:15 +0300

    изменения прописаны для всех пунктов:фамилия,имя,номер,описание.осталась работа с форматами

--- change.py
+++ change.py
@@ -1,7 +1,6 @@
 import write_read
 import logger
-import re
-
+import csv
 
 def askAboutChanging(): 
     questionC = input('''Что вы хотите изменить?
@@ -16,101 +15,61 @@ def askAboutChanging():
             with open('Phonebook_string.csv', 'r+', encoding='utf-8', newline='') as file1:
                 bookString = write_read.read_file_string()
                 record1 = bookString.split()
+                surname = input("Введите фамилию, которую желаете изменить: ") 
                 for i in range(len(record1)):
-                    surname = input("Введите фамилию: ") 
-                    print(record1)
-                    if surname == record1[i]:
-                        index = record1.index(surname)
-                        print(index)
+                    if surname in record1:
                         newSurname = input("Введите новую фамилию абонента: ")
-                        a = (bookString.replace(record1[index], newSurname))
-                        print(a)
+                        a = (bookString.replace(surname, newSurname))
                         print("Фамилия абонента успешно изменена на:", newSurname)
-                        result = pattern.search(i)
-                        if result is None:
-                            file1.write(i)
-
-                
-         
-
-
-
-
-
-
-
-
-                    # return
-                    # #     record[i][surname] = write_read(surname)     
+                        file1.write(a)
+                    else:
+                        break
+                logger.info_logger(f'Изменение записи: {newSurname}')
+                return  
+        case '2':
+            with open('Phonebook_string.csv', 'r+', encoding='utf-8', newline='') as file1:
+                bookString = write_read.read_file_string()
+                record1 = bookString.split()
+                description = input("Введите имя, которое желаете изменить: ") 
+                for i in range(len(record1)):
+                    if description in record1[i]:
+                        newName = input("Введите новое имя абонента: ")
+                        b = (bookString.replace(record1[i], newName))
+                        print("Имя абонента успешно изменено на:", newName)
+                        file1.write(b)
+                    else:
+                        break
+                logger.info_logger(f'Изменение записи: {newName}')
                 return 
-        # case '2':
-        #     name = input("Введите имя: ") 
-        #     file = write_read.read_file_string()
-        #     record = file.split()
-        #     for i in range(len(record)):
-        #     if surname == record[i] and name == record[i] and phone_number == record[i] and description == record[i]:
-        #     if search[surname] != err:
-        #         record[i][surname] = write_file_string(surname)
-        #     if search[name] != err:
-        #         record[i][name] = write_file_string(surname)
-        #     if search[phone_number] != err:
-        #         record[i][phone_number] = write_file_string(surname)
-        #     if search[description] != err:
-        #         record[i][description] = write_file_string(surname)        
-        #     return change (name)
-        # case '3': 
-        #     phone_number = input("Введите номер телефона: ") 
-        #     file = write_read.read_file_string()
-        #     record = file.split()
-        #     for i in range(len(record)):
-        #     if surname == record[i] and name == record[i] and phone_number == record[i] and description == record[i]:
-        #     if search[surname] != err:
-        #         record[i][surname] = write_file_string(surname)
-        #     if search[name] != err:
-        #         record[i][name] = write_file_string(surname)
-        #     if search[phone_number] != err:
-        #         record[i][phone_number] = write_file_string(surname)
-        #     if search[description] != err:
-        #         record[i][description] = write_file_string(surname)        
-        #     return change (phone_number) 
-        # case '4':
-        #     description = input("Введите описание контакта: ") 
-        #     file = write_read.read_file_string()
-        #     record = file.split()
-        #     for i in range(len(record)):
-        #     if surname == record[i] and name == record[i] and phone_number == record[i] and description == record[i]:
-        #     if search[surname] != err:
-        #         record[i][surname] = write_file_string(surname)
-        #     if search[name] != err:
-        #         record[i][name] = write_file_string(surname)
-        #     if search[phone_number] != err:
-        #         record[i][phone_number] = write_file_string(surname)
-        #     if search[description] != err:
-        #         record[i][description] = write_file_string(surname)        
-        #     return change (description)
-        # case '0':
+        case '3': 
+            with open('Phonebook_string.csv', 'r+', encoding='utf-8', newline='') as file1:
+                bookString = write_read.read_file_string()
+                record1 = bookString.split()
+                description = input("Введите номер, который желаете изменить: ") 
+                for i in range(len(record1)):
+                    if description in record1[i]:
+                        newNumber = input("Введите новый номер абонента: ")
+                        b = (bookString.replace(record1[i], newNumber))
+                        print("Номер абонента успешно изменено на:", newNumber)
+                        file1.write(b)
+                    else:
+                        break
+                logger.info_logger(f'Изменение записи: {newNumber}')
+                return 
+        case '4':
+            with open('Phonebook_string.csv', 'r+', encoding='utf-8', newline='') as file1:
+                bookString = write_read.read_file_string()
+                record1 = bookString.split()
+                description = input("Введите старое описание абонента: ") 
+                for i in range(len(record1)):
+                    if description in record1[i]:
+                        newDescription = input("Введите новое описание абонента: ")
+                        b = (bookString.replace(record1[i], newDescription))
+                        print("Описание абонента успешно изменено на:", newDescription)
+                        file1.write(b)
+                    else:
+                        break
+                logger.info_logger(f'Изменение записи: {newDescription}')
+                return 
+        case '0':
             return
-
-               # logger.info_logger(f'Удаление записи: {pattern}')
-
-# def change(record):
-    
-#         if surname == record[i] and name == record[i] and phone_number == record[i] and description == record[i]:
-#             if search[surname] != err:
-#                 record[i][surname] = write_file_string(surname)
-#             if search[name] != err:
-#                 record[i][name] = write_file_string(surname)
-#             if search[phone_number] != err:
-#                 record[i][phone_number] = write_file_string(surname)
-#             if search[description] != err:
-#                 record[i][description] = write_file_string(surname)
-#         return record
-
-# # запись изменений
-# def writer_too(phone_numbers):
-#     with open('names.csv', 'w', encoding='utf-8', newline='') as csvfile:
-#         fieldnames = ['first_name', 'contact info']
-#         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-#         writer.writeheader()
-#         for key in phone_numbers.keys():
-#             writer.writerow({'first_name': key, 'contact info': phone_numbers[key]})
